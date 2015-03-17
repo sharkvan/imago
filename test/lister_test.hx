81cb42d316de51ec6249bffa855881cdd1edd85b
Matt C. Wilson <matthew.c.wilson@gmail.com>
2015-03-16 22:38:10 -0400

    Add tests.

---
+++ test/lister_test.hx
@@ -0,0 +1,30 @@
+package test;
+
+import haxe.unit.TestCase;
+using StringTools;
+
+class Lister_Test extends TestCase
+{
+    // It ensures returned urls are fully qualified
+    @:access(Lister)
+    public function testUrlsAreFullyQualified() {
+        var host = "http://www.example.com";
+        var actual = Lister.clean("/a.png", host);
+        assertTrue(actual.startsWith(host));
+    }
+
+    // It ensures paths are clean
+    @:access(Lister)
+    public function testUrlsAreClean() {
+        var host = "http://www.example.com/";
+        var actual = Lister.clean("/a.png", host).replace("http://", "");
+        assertEquals(actual.indexOf("//"), -1);
+    }
+
+    // It finds the Google daily logo
+    public function testItFindsImgs() {
+        var host = "http://www.google.com/";
+        var actual = Lister.get(host);
+        assertTrue(actual.length >= 1);
+    }
+}
